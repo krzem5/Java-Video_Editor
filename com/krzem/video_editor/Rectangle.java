0ed3d29e7aa9e802c4e88b36925edd19d1af0310
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 17:49:05 +0100

    Push Update

---
+++ com/krzem/video_editor/Rectangle.java
@@ -0,0 +1,20 @@
+package com.krzem.video_editor;
+
+
+
+public class Rectangle{
+	public int x;
+	public int y;
+	public int w;
+	public int h;
+	public boolean neg=false;
+
+
+
+	public Rectangle(int x,int y,int w,int h){
+		this.x=x;
+		this.y=y;
+		this.w=w;
+		this.h=h;
+	}
+}
