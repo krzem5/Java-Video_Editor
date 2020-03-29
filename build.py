960656034655c92ff8da8f44c6f1ee8e73108460
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-29 17:45:07 +0200

    Push Update 03/29/2020, 15:59:21

--- build.py
+++ build.py
@@ -34,29 +34,41 @@ def error(cmd,dc=""):
 
 
 def jar(nm,mp,*dl):
+	def _copy_jar(zf,jfp,wf):
+		with zipfile.ZipFile(jfp,"r") as jf:
+			for nm in jf.namelist():
+				if (nm.upper()!="META-INF/MANIFEST.MF" and len(jf.read(nm))!=0 and nm not in wf):
+					wf+=[nm]
+					print(nm)
+					zf.writestr(nm,jf.read(nm))
+		return wf
 	with zipfile.ZipFile(nm+".jar","w") as zf:
-		zf.write(mp,arcname="META-INF/MANIFEST.MF")
 		wf=[]
 		for d in dl:
-			bp=ntpath.abspath(d)+"\\"
+			bp=(ntpath.abspath(d)+"\\" if len(d.split("|"))==1 else ntpath.abspath(d.split("|")[0])+"\\").replace("\\","/")
+			if (len(d.split("|"))>1):
+				d=d.split("|")[1]
 			for r,_,fl in os.walk(d):
 				for f in fl:
-					if (not ntpath.abspath(ntpath.join(r,f)).replace(bp,"") in wf):
-						zf.write(ntpath.abspath(ntpath.join(r,f)),arcname=ntpath.abspath(ntpath.join(r,f)).replace(bp,""))
-						wf+=[ntpath.abspath(ntpath.join(r,f)).replace(bp,"")]
-						print(ntpath.abspath(ntpath.join(r,f)).replace(bp,""))
+					if (f.endswith(".jar")):
+						wf=_copy_jar(zf,ntpath.abspath(ntpath.join(r,f)),wf)
+					elif (not ntpath.abspath(ntpath.join(r,f)).replace("\\","/").replace(bp,"") in wf):
+						zf.write(ntpath.abspath(ntpath.join(r,f)),arcname=ntpath.abspath(ntpath.join(r,f)).replace("\\","/").replace(bp,""))
+						wf+=[ntpath.abspath(ntpath.join(r,f)).replace("\\","/").replace(bp,"")]
+						print(ntpath.abspath(ntpath.join(r,f)).replace("\\","/").replace(bp,""))
+		zf.write(mp,arcname="META-INF/MANIFEST.MF")
 
 
 
 os.system("rm -rf build&&mkdir build")
-error(["javac","-cp","./build/;./jar/;","-d","build","com/krzem/video_editor/*.java"])
+error(["javac","-cp","./build/;./com/krzem/video_editor/modules/opencv-420.jar;","-d","build","com/krzem/video_editor/*.java"])
 for r,_,fl in os.walk("./com/krzem/video_editor/effects/"):
 	for f in fl:
 		make_dir(f"build/{r}")
-		error(["javac","-cp","./build/;./jar/;","-d",f"build/{r}",ntpath.join(r,f)])
+		error(["javac","-cp","./build/;./com/krzem/video_editor/modules/opencv-420.jar;","-d",f"build/{r}",ntpath.join(r,f)])
 with open("./_tmp_m.tmp","w") as f:
 	f.write("Manifest-Version: 1.0\nCreated-By: Krzem\nMain-Class: com.krzem.video_editor.Main\n")
-jar("video_editor","./_tmp_m.tmp","build","jar")
+jar("video_editor","./_tmp_m.tmp","build",".|com/krzem/video_editor/modules")
 os.system("del _tmp_m.tmp&&rm -rf build&&mkdir build&&copy video_editor.jar build&&del video_editor.jar&&cls")
 os.system("cd build&&java -jar video_editor.jar")
 os.system("cd ../")
